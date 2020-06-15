209c09fc57790133588f7f75eb53694fe2ac8717
Ian Gregg <iangregg188@gmail.com>
2020-06-15 11:58:20 -0700

    Updated user interface logic in the JS and updated DOM to reflect that.
    Co-authored-by: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -24,24 +24,39 @@ PlacesIveBeenBook.prototype.findplace = function(id) {
 
 PlacesIveBeenBook.prototype.deletePlace = function(id) {
   for (let i=0; i< this.places.length; i++) {
-    if (this.places[i].id === id) {
-      delete.this.places;
-      return true;
+    if (this.places[i]) {
+      if (this.places[i].id === id) {
+        delete this.places;
+        return true;
+      }
     }
   };
   return false;
 }
 
 // Business Logic for Place
-function Places(landMark, date, notes) {
+function Places(cityName, landMark, date, notes) {
+  this.cityName = cityName;
   this.landMark = landMark;
   this.date = date;
   this.notes = notes;
 }
+Places.prototype.placeName = function() {
+  return this.cityName;
+}
 
 //User Interface logic
+let placesIveBeenBook = new PlacesIveBeenBook ();
+
 $(document).ready(function() {
-  $('form#new-place').submit(function(event)) {
-event.preventDefault
-  }
+  $('form#new-place').submit(function(event) {
+  event.preventDefault();
+  const inputtedplace = $("input#new-place").val();
+  const inputtedLandMark = $("input#new-landmark").val();
+  const inputtedDate = $("input#new-date").val();
+  const inputtedNotes = $("input#new-notes").val();
+  let newPlace = new Places(inputtedLandMark. inputtedDate, inputtedNotes)
+  placesIveBeenBook.addPlace(newPlace)
+  console.log(placesIveBeenBook.places)
+  });
 });
