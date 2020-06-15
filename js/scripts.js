1e1c7c627c67a071b62c64e1423c6b6ad6da45af
Ian Gregg <iangregg188@gmail.com>
2020-06-15 10:58:50 -0700

    Has updated JS logic through Object orinatated programming.
    Co-authored-by: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
-//business Logic
+//business Logic for places I've been
+function PlacesIveBeenBook() {
+  this.places = []
+  this.currentId = 0
+}
 
+PlacesIveBeenBook.prototype.addPlace = function (place) {
+  place.id = this.assignId();
+  this.places.push(place);
+}
 
+// Business Logic for Place
+function Places(landMark, date, notes) {
+  this.landMark = landMark;
+  this.date = date;
+  this.notes = notes;
+}
 
-//UI logic
+//User Interface logic
 $(document).ready(function(){
 
 });
